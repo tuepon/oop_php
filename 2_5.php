604d10172ce3458a81d9802439bdbc26a2dee69e
Tomonori Ueno <t.uepon@gmail.com>
2018-08-14 00:16:54 +0900

    MAGIC METHOD added.

--- 2_5.php
+++ 2_5.php
@@ -12,13 +12,31 @@
                   return $this->name;
             }
 
-            public function setName(){
+            public function setName($name){
                   $this->name = $name;
+            }
+
+            // __get MAGIC METHOD
+            public function __get($property){
+                  if(property_exists($this, $property)){
+                        return $this->$property;
+                  }
+            }
+
+            // __set MAGIC METHOD
+            public function __set($property, $value){
+                  if(propery_exists($this, $property)){
+                        $this->$property = $value;
+                  }
+                  return $this;
             }
     
       }
     
     $user1 = new User('John, 40');
 
-    echo $user1->getName();
-    
+//     echo $user1->setNmae('Jeff');
+//     echo $user1->getName();
+
+      $user1->__set('age', 41);
+      echo $user1->__get('age');
